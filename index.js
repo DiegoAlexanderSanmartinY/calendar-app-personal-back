627c2e6069609bba09486eaf057eea3f9fbe0dc7
DiegoAlexanderSanmartinY <dasanmartin.g@gmail.com>
2024-03-14 11:25:32 -0500

    index: visuliza puerto

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ app.use(express.static("public"));
 app.use(express.json());
 
 //ROUTES
-//TODO: auth // crear, login, renew
+//auth // crear, login, renew
 app.use("/api/auth", require("./routes/auth"));
-//TODO: events // create, get, update, delete
+//events // create, get, update, delete
 app.use("/api/events", require("./routes/events"));
 
 app.listen(process.env.PORT, () => {
-  console.log("Server running on port 4000");
+  console.log(`Server running on port ${process.env.PORT}`);
 });
